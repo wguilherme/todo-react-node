5d27b7357b8ba3959ea573f67d779c18e955fbf4
W. Guilherme <wguilhermecriativo@gmail.com>
2020-07-04 23:17:18 -0300

    [Update] Task Validations

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const TaskController = require("../controller/TaskController");
 
-router.post("/", TaskController.create);
+const TaskController = require("../controller/TaskController");
+const TaskValidation = require("../middleware/TaskValidation");
+
+router.post("/", TaskValidation, TaskController.create);
 
 module.exports = router;
