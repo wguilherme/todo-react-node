5d4e046aa1cc37eb38bb9d9c41c718fe3036842e
W. Guilherme <wguilhermecriativo@gmail.com>
2020-07-04 23:22:57 -0300

    [Update] Update Task and return new with PUT

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -3,13 +3,28 @@ const TaskModel = require("../model/TaskModel");
 class TaskController {
   async create(req, res) {
     const task = new TaskModel(req.body);
-    
-    await task.save().then((response) => {
+
+    await task
+      .save()
+      .then((response) => {
         console.log("entrou");
         return res.status(200).json(response);
       })
       .catch((error) => {
         return res.status(500, json(error));
+      });
+  }
+
+  async update(req, res) {
+    console.log("entrou");
+    await TaskModel.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+    })
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
       });
   }
 }
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -5,5 +5,6 @@ const TaskController = require("../controller/TaskController");
 const TaskValidation = require("../middleware/TaskValidation");
 
 router.post("/", TaskValidation, TaskController.create);
+router.put("/:id", TaskController.update);
 
 module.exports = router;
