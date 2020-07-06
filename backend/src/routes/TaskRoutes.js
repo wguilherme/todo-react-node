5f259ba1466777eae032af1f5bc75a0cd8e0c7da
W. Guilherme <wguilhermecriativo@gmail.com>
2020-07-05 21:15:45 -0300

    [Update] Update Status (done true/false) by ID

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -5,10 +5,16 @@ const TaskController = require("../controller/TaskController");
 const TaskValidation = require("../middleware/TaskValidation");
 const MacAddressValidation = require("../middleware/MacAddressValidation");
 
+//create
 router.post("/", TaskValidation, TaskController.create);
+//show
 router.get("/:id", TaskController.show);
+//update
 router.put("/:id", TaskController.update);
+//delete
 router.delete("/:id", TaskController.delete);
+//status
+router.put("/:id/:done", TaskController.done);
 
 router.get("/filter/all", MacAddressValidation, TaskController.all);
 
